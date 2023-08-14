95ec91b68faf1bcd94e170f8ad424b2712785e16
alex-untitled <142267266+alex-untitled@users.noreply.github.com>
2023-08-15 00:28:27 +0300

    Update task-list.js (#3)

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -2,22 +2,15 @@ import "./task-list.css";
 import TaskListItem from "../task-list-item/task-list-item";
 
 const TaskList = ({data, onDeleteItem, onToggle}) => {
-  const elements = data.map(item => {
-    const {id, ...rest} = item;
-    return (
-      <TaskListItem 
-      {...rest} 
-      key={id} 
-      onDeleteItem={() => onDeleteItem(id)}
-      onToggle={(e) => onToggle(id, e.currentTarget.getAttribute('data-toggle'))}
-      />
-    )
-  })
-
   return (
     <div className="box">
       <ul className="list-task">
-        {elements}
+        {data.map(task =>    <TaskListItem 
+          {...task} 
+          key={task.id} 
+          onDeleteItem={() => onDeleteItem(id)}
+          onToggle={(e) => onToggle(id, e.currentTarget.getAttribute('data-toggle'))}
+          />)}
       </ul>
     </div>
   );
